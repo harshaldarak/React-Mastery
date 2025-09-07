2405bb54cba6321794a47d022bbd1fa0c3dfc927
Harshal Darak <harshalddarak09@gmail.com>
2025-09-07 21:17:59 +0530

    context api introduction project

---
+++ 08minContext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import UserContext from "../context/userContext";
+function Profile() {
+  const { user } = useContext(UserContext)
+  if (!user) return <div> please log in </div>
+  return <div>welcome {user.username} </div>
+}
+
+export default Profile;
